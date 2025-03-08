2f96230adb2605b9a23a18a762122cb79ee4ff2f
Yash Dhumal <yashdhumal8484@gmail.com>
2025-03-08 22:07:28 +0530

    Normalize line endings

---
+++ backend/dist/UserManager.js
@@ -0,0 +1,57 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserManager = void 0;
+class UserManager {
+    constructor() {
+        this.rooms = new Map();
+    }
+    addUser(name, userId, roomId, socket) {
+        var _a;
+        if (!this.rooms.get(roomId)) {
+            this.rooms.set(roomId, {
+                users: []
+            });
+        }
+        (_a = this.rooms.get(roomId)) === null || _a === void 0 ? void 0 : _a.users.push({
+            id: userId,
+            name,
+            conn: socket
+        });
+        socket.on('close', (reasonCode, description) => {
+            this.removeUser(roomId, userId);
+        });
+    }
+    removeUser(roomId, userId) {
+        var _a;
+        console.log("removed user");
+        const users = (_a = this.rooms.get(roomId)) === null || _a === void 0 ? void 0 : _a.users;
+        if (users) {
+            users.filter(({ id }) => id !== userId);
+        }
+    }
+    getUser(roomId, userId) {
+        var _a;
+        const user = (_a = this.rooms.get(roomId)) === null || _a === void 0 ? void 0 : _a.users.find((({ id }) => id === userId));
+        return user !== null && user !== void 0 ? user : null;
+    }
+    broadcast(roomId, userId, message) {
+        const user = this.getUser(roomId, userId);
+        if (!user) {
+            console.error("User not found");
+            return;
+        }
+        const room = this.rooms.get(roomId);
+        if (!room) {
+            console.error("Room not found");
+            return;
+        }
+        room.users.forEach(({ conn, id }) => {
+            if (id === userId) {
+                return;
+            }
+            console.log("outgoing message " + JSON.stringify(message));
+            conn.sendUTF(JSON.stringify(message));
+        });
+    }
+}
+exports.UserManager = UserManager;
---
+++ backend/src/store/InMemoryStore.js
@@ -0,0 +1,55 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.InMemoryStore = void 0;
+let globalChatId = 0;
+class InMemoryStore {
+    constructor() {
+        this.store = new Map();
+    }
+    initRoom(roomId) {
+        this.store.set(roomId, {
+            roomId,
+            chats: []
+        });
+    }
+    getChats(roomId, limit, offset) {
+        const room = this.store.get(roomId);
+        if (!room) {
+            return [];
+        }
+        return room.chats.reverse().slice(0, offset).slice(-1 * limit);
+    }
+    addChat(userId, name, roomId, message) {
+        if (!this.store.get(roomId)) {
+            this.initRoom(roomId);
+        }
+        const room = this.store.get(roomId);
+        if (!room) {
+            return;
+        }
+        const chat = {
+            id: (globalChatId++).toString(),
+            userId,
+            name,
+            message,
+            upvotes: []
+        };
+        room.chats.push(chat);
+        return chat;
+    }
+    upvote(userId, roomId, chatId) {
+        const room = this.store.get(roomId);
+        if (!room) {
+            return;
+        }
+        const chat = room.chats.find(({ id }) => id == chatId);
+        if (chat) {
+            if (chat.upvotes.find(x => x === userId)) {
+                return chat;
+            }
+            chat.upvotes.push(userId);
+        }
+        return chat;
+    }
+}
+exports.InMemoryStore = InMemoryStore;
